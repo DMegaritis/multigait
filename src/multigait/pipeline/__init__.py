071579399bbe16c8c7b84c4429017993db1d91b4
DMegaritis <dmgrei3@gmail.com>
2026-06-26 16:04:17 +0100

    refactored imports to align with updated `multigait` package structure across tests, examples, and core modules

--- src/multigait/pipeline/__init__.py
+++ src/multigait/pipeline/__init__.py
@@ -1,5 +1,13 @@
-from multigait.pipeline.multimobility_pipeline import MultiGaitPipeline, MultiGaitPipelineMultimorbidityImpaired, MultiGaitPipelineHealthyCoMorbidity
+from multigait.pipeline.multimobility_pipeline import (
+    MultiGaitPipeline,
+    MultiGaitPipelineMultimorbidityImpaired,
+    MultiGaitPipelineHealthyCoMorbidity,
+)
 from multigait.pipeline.pipeline_base import PipelineBase
 
-__all__ = ["MultiGaitPipeline", "MultiGaitPipelineMultimorbidityImpaired", "MultiGaitPipelineHealthyCoMorbidity",
-           "PipelineBase"]
+__all__ = [
+    "MultiGaitPipeline",
+    "MultiGaitPipelineMultimorbidityImpaired",
+    "MultiGaitPipelineHealthyCoMorbidity",
+    "PipelineBase",
+]
